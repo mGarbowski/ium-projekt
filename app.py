b93b193bcbaad09eea4f001caeee86859c397783
mgarbowski <mikolaj.garbowski@gmail.com>
2025-05-11 14:03:01 +0200

    use np array in app

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
 import pickle
-import pandas as pd
+from typing import Dict, Union
+
+import numpy as np
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
-from typing import Dict, Union, Optional
+
 
 class PredictRequest(BaseModel):
     user_id: str = Field(..., description="Unique identifier for the user")
     data: Dict[str, Union[float, int, bool]] = Field(..., description="Features for prediction")
+
+
 app = FastAPI()
 
 # Load the model
@@ -19,11 +23,12 @@ def test_predict(features: dict):
     print(features)
     return {"message": "Test prediction endpoint hit!", "features": features}
 
+
 @app.post("/predict/")
 def predict(request: PredictRequest):
     user_id = request.user_id
     features = request.data
-    features_df = pd.DataFrame([features])
-    prediction = model.predict(features_df)
+    features_array = np.array([list(features.values())])
+    prediction = model.predict(features_array)
     print(f"Prediction for user {user_id}: {prediction}")
     return {"prediction": prediction.tolist()}
