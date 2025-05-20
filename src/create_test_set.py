f4585adebe5f033ab411ca8576d8c4e214c54b27
mgarbowski <mikolaj.garbowski@gmail.com>
2025-05-20 13:12:27 +0200

    add script for creating a test set for the A/B experiments

---
+++ src/create_test_set.py
@@ -0,0 +1,31 @@
+"""Create test set for A/B experiment."""
+import pandas as pd
+
+from listings_preprocessing import aggregate_rating_columns
+
+LISTINGS_FILE = "../data/raw/listings.csv"
+TEST_SET_FILE = "../data/processed/test_set.csv"
+
+
+def main():
+    listings = pd.read_csv(LISTINGS_FILE)
+    listings = aggregate_rating_columns(listings)
+
+    columns_absent_in_api_requests = [
+        "last_scraped",
+        "calendar_updated",
+        "calendar_last_scraped",
+        "number_of_reviews",
+        "number_of_reviews_ltm",
+        "number_of_reviews_l30d",
+        "first_review",
+        "last_review",
+        "reviews_per_month",
+    ]
+    listings = listings.drop(columns=columns_absent_in_api_requests, errors="ignore")
+    listings = listings[listings["avg_rating"].notna()]
+    listings.to_csv(TEST_SET_FILE, index=False)
+
+
+if __name__ == "__main__":
+    main()
